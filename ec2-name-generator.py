74feeaeb677a903af31030ac2ea9aa3c900f3b5a
Wrijlarr <wrijlarr@outlook.com>
2025-11-02 12:47:40 -0500

    Department name and instances input

--- ec2-name-generator.py
+++ ec2-name-generator.py
@@ -4,3 +4,8 @@ import random
 
 print("EC2 Instance Generator")
 
+## --- Department Name and Number of Instances ---
+
+department_name = input("Department Name: ")
+
+ec2_numbers = input("EC2 Instances to be created?: ")
