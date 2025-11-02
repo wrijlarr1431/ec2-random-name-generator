ebc8799523aea5b378a0b51f82cc8d2fd2400cc7
Wrijlarr <wrijlarr@outlook.com>
2025-11-02 14:32:28 -0500

    code update for EC2 generation

--- ec2-name-generator.py
+++ ec2-name-generator.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 ## --- Welcome Screen ---
 
@@ -8,9 +9,18 @@ print("EC2 Instance Generator")
 
 department_name = input("Department Name: ")
 
-ec2_numbers = input("EC2 Instances to be created?: ")
+ec2_numbers = input("EC2 Instances to be created: ")
 
 ## --- Generate EC2 Names ---
 
 print(f"\nEC2 Names for {department_name}:")
 print("-" * 40)
+
+for i in range(int(ec2_numbers)):
+    # Generate Unique 8 character string
+    random_chars = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+
+    #Create EC2 name
+    ec2_name = f"{department_name.upper()}-EC2-{random_chars}"
+
+    print(f"{i+1}.{ec2_name}")
