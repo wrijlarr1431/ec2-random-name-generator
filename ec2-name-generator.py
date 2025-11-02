8f0d5c7d931a0cca5ec5634b4ab6ff6bc0b8a4df
Wrijlarr <wrijlarr@outlook.com>
2025-11-02 14:10:18 -0500

    Generate EC2 Names section

--- ec2-name-generator.py
+++ ec2-name-generator.py
@@ -9,3 +9,8 @@ print("EC2 Instance Generator")
 department_name = input("Department Name: ")
 
 ec2_numbers = input("EC2 Instances to be created?: ")
+
+## --- Generate EC2 Names ---
+
+print(f"\nEC2 Names for {department_name}:")
+print("-" * 40)
